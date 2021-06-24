ab0bf797bd913eb8355c6aaf85fdc3168778adc3
unknown <csmcc2@hotmail.com>
2021-06-24 14:35:55 +0800

    version

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import STIcon from '@ant-design/icons/YoutubeFilled'
 import RadikoIcon from '@ant-design/icons/TrademarkCircleFilled'
 
 const { Header, Footer, Content } = Layout
-const version: string = "v20210616"
+const version: string = "v20210624"
 
 export default function App() {
   const [title, setTitle] = useState<string>("Picture")
